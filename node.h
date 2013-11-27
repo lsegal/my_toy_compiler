3d3255a26fd2b17952598830f2a88f2d14bf3b66
Loren Segal <lsegal@amazon.com>
2013-11-26 23:28:38 -0800

    Fix invalid virtual function declaration

--- node.h
+++ node.h
@@ -14,7 +14,7 @@ typedef std::vector<NVariableDeclaration*> VariableList;
 class Node {
 public:
 	virtual ~Node() {}
-	virtual llvm::Value* codeGen(CodeGenContext& context);
+	virtual llvm::Value* codeGen(CodeGenContext& context) { return NULL; }
 };
 
 class NExpression : public Node {
